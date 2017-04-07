ae02f64f768604d0de6483be5527e232e31a4352
Tu Ngo <tungo8810@gmail.com>
2017-04-07 11:54:07 -0700

    finished ResizingIntSet

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -68,18 +68,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if @count < num_buckets
+      self[num] << num
+      @count += 1
+    else
+      resize!
+      self.insert(num)
+    end
   end
 
   def remove(num)
+    @count -= 1
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -87,5 +98,14 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_store[num % (num_buckets * 2)] << num
+      end
+    end
+
+    @store = new_store
   end
 end
