5be5cd149f1286a2228648bd49e6f929378bd8cf
Tu Ngo <tungo8810@gmail.com>
2017-04-07 11:09:42 -0700

    Add MaxIntSet class

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
+    @max = max
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num < @max
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
