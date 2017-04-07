e70bca8c9be1ad1c90008f93f3e0d1153440ecfb
Tu Ngo <tungo8810@gmail.com>
2017-04-07 14:07:50 -0700

    Add hash method

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,14 +1,29 @@
 class Fixnum
-  # Fixnum#hash already implemented for you
+
 end
 
 class Array
   def hash
+    result = 0
+
+    each_with_index do |el, idx|
+      result = result ^ (el + idx)
+    end
+
+    result
   end
 end
 
 class String
   def hash
+    result = 0
+    az = ('A'..'z').to_a + [' ']
+
+    chars.each_with_index do |el, idx|
+      result = result ^ (az.index(el) + idx)
+    end
+
+    result
   end
 end
 
@@ -16,6 +31,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    result = 0
+
+    each do |key, value|
+      result = result ^ (key.hash + value.hash)
+    end
+
+    result
   end
 end
