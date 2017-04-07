3b430a09d164c360777fcce65c14e62ef718b4c4
Tu Ngo <tungo8810@gmail.com>
2017-04-07 15:39:23 -0700

    linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -15,11 +15,18 @@ class Link
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @next.prev = @prev unless @next.nil?
+    @prev.next = @next unless @prev.nil?
+    @prev, @next = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = nil
+    @tail = nil
   end
 
   def [](i)
@@ -28,34 +35,69 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
   def empty?
+    first.nil?
   end
 
   def get(key)
+    each do |link|
+      return link.val if link.key == key
+    end
   end
 
   def include?(key)
+    get(key) != nil
   end
 
   def append(key, val)
+    link = Link.new(key, val)
+    if @head.nil?
+      link.prev = nil
+      @head = link
+      @tail = link
+    else
+      link.prev = @tail
+      @tail.next = link
+      @tail = link
+    end
   end
 
   def update(key, val)
+    each do |link|
+      link.val = val if link.key = key
+    end
   end
 
   def remove(key)
+    link = nil
+    each do |lk|
+      link = lk if lk.key == key
+    end
+
+    unless link.nil?
+      @head = link.next if link == @head
+      @tail = link.prev if link == @tail
+      link.remove
+    end
   end
 
   def each
+    link = @head
+    until link.nil?
+      yield(link) if block_given?
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
