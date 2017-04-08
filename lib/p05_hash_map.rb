df2ef063c6037ddae03b131fc1ec91a783029193
Tu Ngo <tungo8810@gmail.com>
2017-04-07 17:04:08 -0700

    Add Hashmap

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,8 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,18 +12,45 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if count > num_buckets
+      resize!
+      if include?(key)
+        bucket(key).update(key, val)
+      else
+        bucket(key).append(key, val)
+        @count += 1
+      end
+    else
+      if include?(key)
+        bucket(key).update(key, val)
+      else
+        bucket(key).append(key, val)
+        @count += 1
+      end
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
   def each
+    @store.each do |list|
+      list.each do |link|
+        yield(link.key, link.val) if block_given?
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +71,20 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { LinkedList.new }
+
+    @store.each do |list|
+      list.each do |link|
+        new_store[link.key.hash % (num_buckets * 2)]
+          .append(link.key, link.val)
+      end
+    end
+
+    @store = new_store
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
